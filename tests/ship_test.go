10706aa4c6356068ed228fae0c998fe43872499f
ebarhami <barhami12345@gmail.com>
2023-07-10 15:42:55 +0700

    add test for ship

--- tests/ship_test.go
+++ tests/ship_test.go
@@ -1,1 +1,21 @@
 package tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"app/game"
+)
+
+func TestHitShip(t *testing.T) {
+	ship := game.NewShip(game.Coordinate{})
+
+	assert.False(t, ship.IsHit)
+	ship.Hit()
+	assert.True(t, ship.IsHit)
+	
+	// should be idempotent
+	ship.Hit()
+	assert.True(t, ship.IsHit)
+}
